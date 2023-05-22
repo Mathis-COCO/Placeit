64eba734385a3c7146dee1c75c907b4c236b6399
Mathis-COCO <mathis.coco@ynov.com>
2023-05-22 11:52:28 +0200

    ui changes - left bar and draggable elements

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,16 @@
 import LeftBar from './components/LeftBar'
+import Options from './components/Options'
+import '../src/assets/css/index.css'
+import InteractZones from './components/Interact'
 
 function App(): JSX.Element {
   return (
     <div className="container">
       <LeftBar />
+      <div className='right-part'>
+        {/* <Options /> */}
+        <InteractZones />
+      </div>
     </div>
   )
 }
---
+++ src/renderer/src/components/Options.tsx
@@ -0,0 +1,18 @@
+import '../assets/css/Options.css'
+
+function Options(): JSX.Element {
+    const optionsList = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5'];
+
+    return (
+        <div className="options-container">
+            {optionsList.map((name, index) => (
+                <div className='options'>
+                    <p key={index} className='option-txt'>{name}</p>
+                    <input type="checkbox" />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Options;
