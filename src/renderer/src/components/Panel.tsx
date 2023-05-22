500b258698219ea51687b145e109a73f15a6b141
pauloducap <paulabeille3302@gmail.com>
2023-05-22 17:48:01 +0200

    zone management sidebar

--- src/renderer/src/components/Panel.tsx
+++ src/renderer/src/components/Panel.tsx
@@ -4,10 +4,25 @@ import {Button} from "./common/Button";
 
 export default function Panel(): JSX.Element {
     const [groups, setGroups] = useState<Array<string>>(["Group 1", "Group 2", "Group 3"]);
+    const [selectedZone, setSelectedZone] = useState<null | string>(null);
+    const [selectedOption, setSelectedOption] = useState<string>("");
 
     const addGroup = () => {
         setGroups([...groups, "Nouveau groupe"])
     }
+
+    const selectZone = (zone: string) => {
+        setSelectedZone(zone);
+    }
+
+    const closeDropdown = () => {
+        setSelectedZone(null);
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedOption(event.target.value);
+    }
+
 
     return (
         <div className="left-bar">
@@ -22,6 +37,27 @@ export default function Panel(): JSX.Element {
                     ))
                 }
             </div>
+            <div className="zones-container">
+                <div className="zone" onClick={() => selectZone("Zone 1")}>
+                    Zone 1
+                </div>
+                <div className="zone" onClick={() => selectZone("Zone 2")}>
+                    Zone 2
+                </div>
+            </div>
+            {selectedZone && (
+                <div className="dropdown-menu">
+                    <button className="close-button" onClick={closeDropdown}>X</button>
+                    <div className="reservation">
+                        <span>Réservation</span>
+                        <select value={selectedOption} onChange={handleChange}>
+                            {["Example", "Test","OUI","NON"].map((option, index) => (
+                                <option key={index} value={option}>{option}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
