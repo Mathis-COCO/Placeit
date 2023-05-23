0eba8e507ead655f467a795e39f3a32431458bd0
vbetsch <vic.betsch@gmail.com>
2023-05-23 12:19:46 +0200

    :lipstick: Refactor CSS

--- src/renderer/src/components/RightPanel.tsx
+++ src/renderer/src/components/RightPanel.tsx
@@ -1,6 +1,7 @@
 import {ChangeEvent, useContext, useState} from "react";
 import {AppContext} from "../context/app/AppProvider";
 import {AppActionType} from "../context/app/AppReducer";
+import deploy from '../assets/img/deploy.svg'
 
 export default function RightPanel(): JSX.Element {
     const [state, dispatch] = useContext(AppContext);
@@ -18,38 +19,33 @@ export default function RightPanel(): JSX.Element {
     }
 
     return state.selectedZone ? (
-
-        <div className="dropdown-menu">
-            <button className="close-button" onClick={closeDropdown}>X</button>
-            <h2>Zone {state.selectedZone.id}</h2>
-            <div className="reservation">
-                <span>Réservation</span>
+        <div className="right-panel">
+            <button className="close-button" onClick={closeDropdown}>
+                <img src={deploy} alt="deploy"/>
+            </button>
+            <h1>Zone {state.selectedZone.id}</h1>
+            <div className="fields">
+                <span className="field-text">Réservation</span>
                 <select value={selectedOption} onChange={handleChange}>
                     {["Example", "Test", "OUI", "NON"].map((option, index) => (
                         <option key={index} value={option}>{option}</option>
                     ))}
                 </select>
-                <div className="option">
-                    <span>Nombre de places</span>
-                    <input className="option-input" type="number" min="0"/>
-                </div>
-                <div className="option">
-                    <span>Table disponible</span>
-                    <label className="switch">
-                        <input type="checkbox"/>
-                        <span className="slider round"></span>
-                    </label>
-                </div>
-                <div className="option">
-                    <span>Statut de nettoyage</span>
-                    <select className="option-select">
-                        {["Clean", "Dirty"].map((option, index) => (
-                            <option key={index} value={option}>
-                                {option}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                <span className="field-text">Nombre de places</span>
+                <input className="option-input" type="number" min="0"/>
+                <span className="field-text">Table disponible</span>
+                <label className="switch">
+                    <input type="checkbox"/>
+                    <span className="slider round"></span>
+                </label>
+                <span className="field-text">Statut de nettoyage</span>
+                <select className="option-select">
+                    {["Clean", "Dirty"].map((option, index) => (
+                        <option key={index} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
             </div>
         </div>
     ) : (<div></div>)
