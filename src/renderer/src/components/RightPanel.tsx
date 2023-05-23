5f084b1f037e269a8e789d248ecf58a029e14909
vbetsch <vic.betsch@gmail.com>
2023-05-23 11:22:30 +0200

    :sparkles: RightPanel form by PA

--- src/renderer/src/components/RightPanel.tsx
+++ src/renderer/src/components/RightPanel.tsx
@@ -18,6 +18,7 @@ export default function RightPanel(): JSX.Element {
     }
 
     return state.selectedZone ? (
+
         <div className="dropdown-menu">
             <button className="close-button" onClick={closeDropdown}>X</button>
             <h2>Zone {state.selectedZone.id}</h2>
@@ -28,6 +29,27 @@ export default function RightPanel(): JSX.Element {
                         <option key={index} value={option}>{option}</option>
                     ))}
                 </select>
+                <div className="option">
+                    <span>Nombre de places</span>
+                    <input className="option-input" type="number" min="0"/>
+                </div>
+                <div className="option">
+                    <span>Table disponible</span>
+                    <label className="switch">
+                        <input type="checkbox"/>
+                        <span className="slider round"></span>
+                    </label>
+                </div>
+                <div className="option">
+                    <span>Statut de nettoyage</span>
+                    <select className="option-select">
+                        {["Clean", "Dirty"].map((option, index) => (
+                            <option key={index} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
         </div>
     ) : (<div></div>)
